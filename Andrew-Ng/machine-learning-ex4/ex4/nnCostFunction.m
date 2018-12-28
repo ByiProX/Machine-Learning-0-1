327731389520247478283c6d29fa8044155997e3
Quentin Wang <wangkx0105@outlook.com>
2018-12-28 16:54:04 +0800

    ex4 gradient almost .......

--- Andrew-Ng/machine-learning-ex4/ex4/nnCostFunction.m
+++ Andrew-Ng/machine-learning-ex4/ex4/nnCostFunction.m
@@ -17,18 +17,18 @@ function [J grad] = nnCostFunction(nn_params, ...
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
 Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+                 hidden_layer_size, (input_layer_size + 1)); % 25 * 401
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+                 num_labels, (hidden_layer_size + 1)); % 10 * 26
 
 % Setup some useful variables
 m = size(X, 1);
 
 % You need to return the following variables correctly
 J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+Theta1_grad = zeros(size(Theta1)); % 25 * 401
+Theta2_grad = zeros(size(Theta2)); % 10 * 26
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -64,6 +64,7 @@ Theta2_grad = zeros(size(Theta2));
 
 
 X = [ones(m, 1) X];
+
 y_new = ones(m, num_labels);
 for line = 1: m
     y_new(line, :) = 1: num_labels;
@@ -74,9 +75,7 @@ size(y_new); % 5000 * 10
 
 % 每一层都要sigmoid计算
 hidden_layer = [ones(m, 1) sigmoid(X * Theta1')]; % 5000 * 26
-
 output_layer = hidden_layer * Theta2'; % 5000 * 10;
-
 h =  sigmoid(output_layer); % 5000 * 10;
 
 % ====================== J without regularization ======================
@@ -90,7 +89,7 @@ h =  sigmoid(output_layer); % 5000 * 10;
 %% 方法 2：wiithout regularization
 % y = y_new;
 % j_matrix = -(y .* log(h) + (1 - y) .* log(1 - h));
-% J = sum(sum(j_matrix'))/5000;
+% J = sum(sum(j_matrix'))/m;
 
 
 % ====================== J with regularization ======================
@@ -102,15 +101,40 @@ reg_matrix = sum(sum(Theta1(:,[2:end]) .^ 2)) + sum(sum(Theta2(:,[2:end]) .^ 2))
 J_2 = reg_matrix * lambda / (2*m);
 J = J_1 + J_2;
 
+% ======================    backpropagation    ======================
+delta_3 = h - y_new; % 5000 * 10
+% fprintf('size of delta3 %d %d \n' , size(delta_3));
+delta_2 = delta_3 * Theta2 .* sigmoidGradient(hidden_layer); % 5000 * 26
+% fprintf('size of delta2 %d %d \n' , size(delta_2));
+Dt_1 = delta_2(:, 2:end)' * X; % 25 * 401
+% fprintf('size of Dt_1   %d %d \n' , size(Dt_1));
+Dt_2 = delta_3' * hidden_layer; % 10 * 26
+% fprintf('size of Dt_2   %d %d \n' , size(Dt_2));
+Theta1_grad = Dt_1 / m;
+Theta2_grad = Dt_2 / m;
+
+
+
+
+% Dt_1 = zeros(hidden_layer_size, input_layer_size + 1); % 25 * 401
+% Dt_2 = zeros(num_labels, hidden_layer_size + 1); % 10 * 26
+% for c = 1: m
+%     delta_3 = h(c,:)' - y_new(c,:)'; % 10 * 1
+%     delta_2 = Theta2' * delta_3 .* sigmoidGradient(hidden_layer(c, :)'); % 26 * 1
+%     Dt_1 += delta_2(2:end) * X(c, :);
+%     Dt_2 += delta_3 * hidden_layer(c,:);
+%
+% end
+%
+% Theta1_grad = Dt_1 / m;
+% Theta2_grad = Dt_2 / m;
+% --------------------  regularized part ------------------------
 
 
 
 
 
 
-
-
-% -------------------------------------------------------------
 
 % =========================================================================
 
