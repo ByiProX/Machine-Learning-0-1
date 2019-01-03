e9c7c0e10a1dd34183d88cbee634440f43b35a5b
Quentin Wang <wangkx0105@outlook.com>
2019-01-03 09:25:12 +0800

    ex6 dataset3params finished

--- Andrew-Ng/machine-learning-ex6/ex6/dataset3Params.m
+++ Andrew-Ng/machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,13 +15,49 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+% C_set = logspace(0, 2, 3);
+% sigma_set = logspace(-1, 1, 3);
+
+C_set = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_set = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+error = 0;
+init_flag = 0;
+
+
+for C = C_set
+    for sigma = sigma_set
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        if init_flag == 0
+            error = mean(double(predictions ~= yval));
+            init_flag = 1;
+            continue;
+        end
+
+        if mean(double(predictions ~= yval)) < error
+            error = mean(double(predictions ~= yval));
+            target_C = C;
+            target_sigma = sigma;
+        end
+    end
+end
+
+
+
+
+
+C = target_C;
+sigma = target_sigma;
+
+
+
 
 
 
